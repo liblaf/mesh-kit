fix: update data types in points.py for better precision
Update data types in the `position_to_index` function in `points.py` to use more precise floating point types for `distance` and `index`, improving accuracy and precision in calculations.

src/mkit/array/points.py
@@ -5,7 +5,7 @@ from scipy import spatial
 
 def position_to_index(
     points: npt.ArrayLike, pos: npt.ArrayLike, *, distance_upper_bound: float = np.inf
-) -> npt.NDArray[np.intp]:
+) -> npt.NDArray[np.integer]:
     """
     Args:
         points: (V, 3) float
@@ -18,7 +18,28 @@ def position_to_index(
     points = np.asarray(points)
     pos = np.asarray(pos)
     kdtree = spatial.KDTree(points)
-    distance: npt.NDArray[np.float64]
-    index: npt.NDArray[np.intp]
+    distance: npt.NDArray[np.floating]
+    index: npt.NDArray[np.integer]
     distance, index = kdtree.query(pos, distance_upper_bound=distance_upper_bound)
     return index
+
+
+def position_to_mask(
+    points: npt.ArrayLike, pos: npt.ArrayLike, *, distance_upper_bound: float = np.inf
+) -> npt.NDArray[np.bool_]:
+    """
+    Args:
+        points: (V, 3) float
+        pos: (N, 3) float
+        distance_upper_bound: float
+
+    Returns:
+        (V,) bool
+    """
+    points = np.asarray(points)
+    mask: npt.NDArray[np.bool_] = np.zeros((len(points),), np.bool_)
+    idx: npt.NDArray[np.intp] = position_to_index(
+        points, pos, distance_upper_bound=distance_upper_bound
+    )
+    mask[idx] = True
+    return mask

src/mkit/ops/tetgen.py
@@ -25,23 +25,14 @@ def tetgen(mesh: meshio.Mesh, *, verbose: bool = True) -> meshio.Mesh:
         tmpdir = pathlib.Path(tmpdir)
         input_file: pathlib.Path = tmpdir / "mesh.smesh"
         save_smesh(input_file, mesh)
-        args: list[str] = ["tetgen", "-p", "-q", "-O", "-z", "-k", "-C"]
+        args: list[str] = ["tetgen", "-p", "-q", "-O", "-z", "-k", "-C", "-X"]
         if verbose:
             args.append("-V")
         subprocess.run([*args, input_file], check=True)
         tetra_mesh: meshio.Mesh = meshio.read(tmpdir / "mesh.1.vtk")
         points: npt.NDArray[np.floating] = tetra_mesh.points
         tetra: npt.NDArray[np.intp] = tetra_mesh.get_cells_type("tetra")
-        faces: npt.NDArray[np.intp]
-        boundary_marker: npt.NDArray[np.intp]
-        faces, boundary_marker = load_face(tmpdir / "mesh.1.face")
-        return meshio.Mesh(
-            points=points,
-            cells=[("tetra", tetra), ("triangle", faces)],
-            cell_data={
-                "boundary_marker": [np.zeros(len(tetra), np.intp), boundary_marker]
-            },
-        )
+        return meshio.Mesh(points=points, cells=[("tetra", tetra)])
 
 
 def load_face(

tasks/00-register/src/align.py
@@ -3,6 +3,7 @@ from typing import Annotated, Optional
 
 import meshio
 import mkit.array.mask
+import mkit.array.points
 import mkit.cli
 import mkit.io
 import mkit.ops.register.icp
@@ -52,15 +53,19 @@ def main(
     target: meshio.Mesh
     source_masked: trimesh.Trimesh
     target_masked: trimesh.Trimesh
-    source, source_masked = load_mesh_masked(source_file)
-    target, target_masked = load_mesh_masked(target_file)
+    source_landmarks: npt.NDArray[np.integer]
+    target_landmarks: npt.NDArray[np.integer]
+    source, source_masked, source_landmarks = load_mesh_masked(
+        source_file, source_landmarks_file
+    )
+    target, target_masked, target_landmarks = load_mesh_masked(
+        target_file, target_landmarks_file
+    )
     initial: npt.NDArray[np.floating] | None = None
     cost: float
     if initial_transform_file:
         initial = np.load(initial_transform_file)
-    elif source_landmarks_file and target_landmarks_file:
-        source_landmarks: npt.NDArray[np.floating] = np.loadtxt(source_landmarks_file)
-        target_landmarks: npt.NDArray[np.floating] = np.loadtxt(target_landmarks_file)
+    elif len(source_landmarks) > 5:
         initial, cost = trimesh.registration.procrustes(
             source_landmarks, target_landmarks
         )
@@ -81,11 +86,11 @@ def main(
 
 
 def load_mesh_masked(
-    file: pathlib.Path,
-) -> tuple[meshio.Mesh, trimesh.Trimesh]:
+    file: pathlib.Path, landmarks_file: pathlib.Path | None
+) -> tuple[meshio.Mesh, trimesh.Trimesh, npt.NDArray[np.integer]]:
     mesh_io: meshio.Mesh = mkit.io.load_meshio(file)
     mesh_tr: trimesh.Trimesh = mkit.io.as_trimesh(mesh_io)
-    vert_mask: npt.NDArray[np.bool_] | None = mesh_io.point_data.get("mask", None)
+    vert_mask: npt.NDArray[np.bool_] | None = mesh_io.point_data.get("register", None)
     if vert_mask is not None:
         vert_mask = vert_mask.astype(np.bool_)
         face_mask: npt.NDArray[np.bool_] = mkit.array.mask.vertex_to_face(
@@ -93,7 +98,22 @@ def load_mesh_masked(
         )
         mesh_tr.update_faces(face_mask)
         mesh_tr = mesh_tr.process()
-    return mesh_io, mesh_tr
+    landmarks: npt.NDArray[np.integer] = load_landmarks(mesh_io, landmarks_file)
+    return mesh_io, mesh_tr, landmarks
+
+
+def load_landmarks(
+    mesh: meshio.Mesh, landmarks_file: pathlib.Path | None
+) -> npt.NDArray[np.integer]:
+    if "landmarks" in mesh.field_data:
+        return mesh.field_data["landmarks"]
+    if landmarks_file is None or not landmarks_file.exists():
+        return np.empty((0, 3), np.intp)
+    points: npt.NDArray[np.floating] = np.loadtxt(landmarks_file)
+    idx: npt.NDArray[np.integer] = mkit.array.points.position_to_index(
+        mesh.points, points
+    )
+    return idx
 
 
 def align(

tasks/00-register/src/register.py
@@ -27,7 +27,12 @@ def main(
     result: npt.NDArray[np.floating] = trimesh.registration.nricp_amberg(
         source_tr,
         target_tr,
-        steps=[[0.02, 3, 0.5, 10], [0.007, 3, 0.5, 10], [0.002, 3, 0.5, 10]],
+        steps=[
+            [0.02, 3, 0.5, 10],
+            [0.007, 3, 0.5, 10],
+            [0.002, 3, 0.5, 10],
+            # [0.001, 0, 0.0, 10],
+        ],
         distance_threshold=0.05,
     )
     source_io.points = result

tasks/00-register/src/template/face/process.py
@@ -33,7 +33,17 @@ def main(
     )
     mesh.update_faces(~faces_to_remove)
     mesh = mkit.ops.mesh_fix.mesh_fix(mesh, verbose=True)
-    mkit.io.save(output_file, mesh)
+    validation_mask: npt.NDArray[np.bool_] = trimesh.bounds.contains(
+        [[-np.inf, -np.inf, -38.0], [np.inf, 150.0, 80.0]], mesh.vertices
+    )
+    mkit.io.save(
+        output_file,
+        mesh,
+        point_data={
+            "register": np.ones((len(mesh.vertices),), np.int8),
+            "validation": validation_mask.astype(np.int8),
+        },
+    )
 
 
 if __name__ == "__main__":

tasks/00-register/src/template/skull/process.py
@@ -45,12 +45,14 @@ def main(
         skull.vertices,
         method="nearest",
     ).astype(np.bool_)
-    mkit.io.save(output_file, skull, point_data={"mask": skull_mask.astype(np.int8)})
     mkit.io.save(
-        mandible_file, mandible, point_data={"mask": mandible_mask.astype(np.int8)}
+        output_file, skull, point_data={"register": skull_mask.astype(np.int8)}
     )
     mkit.io.save(
-        maxilla_file, maxilla, point_data={"mask": maxilla_mask.astype(np.int8)}
+        mandible_file, mandible, point_data={"register": mandible_mask.astype(np.int8)}
+    )
+    mkit.io.save(
+        maxilla_file, maxilla, point_data={"register": maxilla_mask.astype(np.int8)}
     )
 
 

tasks/01-tetgen/src/tetgen.py
@@ -8,6 +8,7 @@ import mkit.io
 import mkit.ops.mesh_fix
 import mkit.ops.ray
 import mkit.ops.tetgen
+import mkit.tetra
 import numpy as np
 import scipy.interpolate
 import trimesh
@@ -32,42 +33,83 @@ def main(
     *,
     output_file: Annotated[pathlib.Path, typer.Option("-o", "--output", writable=True)],
 ) -> None:
-    pre_face: trimesh.Trimesh = mkit.io.load_trimesh(pre_face_file)
-    pre_mandible: trimesh.Trimesh = mkit.io.load_trimesh(pre_mandible_file)
-    pre_maxilla: trimesh.Trimesh = mkit.io.load_trimesh(pre_maxilla_file)
-    pre_skull: trimesh.Trimesh = trimesh.util.concatenate([pre_mandible, pre_maxilla])
-    post_mandible: trimesh.Trimesh = mkit.io.load_trimesh(post_mandible_file)
-    post_maxilla: trimesh.Trimesh = mkit.io.load_trimesh(post_maxilla_file)
-    post_skull: trimesh.Trimesh = trimesh.util.concatenate(
-        [post_mandible, post_maxilla]
+    pre_face: meshio.Mesh = mkit.io.load_meshio(pre_face_file)
+    pre_mandible: meshio.Mesh = mkit.io.load_meshio(pre_mandible_file)
+    pre_maxilla: meshio.Mesh = mkit.io.load_meshio(pre_maxilla_file)
+    post_mandible: meshio.Mesh = mkit.io.load_meshio(post_mandible_file)
+    post_maxilla: meshio.Mesh = mkit.io.load_meshio(post_maxilla_file)
+    tetra: meshio.Mesh = tetgen(
+        face=mkit.io.as_trimesh(pre_face),
+        mandible=mkit.io.as_trimesh(pre_mandible),
+        maxilla=mkit.io.as_trimesh(pre_maxilla),
     )
+    face_mask: npt.NDArray[np.bool_]
+    skull_mask: npt.NDArray[np.bool_]
+    face_mask, skull_mask = extract_face_skull(tetra)
 
-    pre_face_repaired: trimesh.Trimesh = mkit.ops.mesh_fix.mesh_fix(pre_face)
-    pre_mandible_repaired: trimesh.Trimesh = mkit.ops.mesh_fix.mesh_fix(pre_mandible)
-    pre_maxilla_repaired: trimesh.Trimesh = mkit.ops.mesh_fix.mesh_fix(pre_maxilla)
-    pre_skull_repaired: trimesh.Trimesh = trimesh.boolean.union(
-        [pre_mandible_repaired, pre_maxilla_repaired]
-    )
-    pre_repaired: trimesh.Trimesh = trimesh.util.concatenate(
-        [pre_face_repaired, pre_skull_repaired]
-    )
-    hole: npt.NDArray[np.floating] = mkit.ops.ray.find_inner_point(pre_skull_repaired)
-    tetra: meshio.Mesh = mkit.ops.tetgen.tetgen(
-        mkit.io.as_meshio(pre_repaired, field_data={"holes": [hole]})
-    )
-
-    closest: npt.NDArray[np.floating]
-    distance: npt.NDArray[np.floating]
-    triangle_id: npt.NDArray[np.integer]
-    closest, distance, triangle_id = pre_skull_repaired.nearest.on_surface(tetra.points)
-    skull_mask: npt.NDArray[np.bool_] = distance < 1e-6 * pre_skull.scale
-    disp_origin: npt.NDArray[np.floating] = post_skull.vertices - pre_skull.vertices
-    disp_interp: npt.NDArray[np.floating] = scipy.interpolate.griddata(
-        pre_skull.vertices, disp_origin, tetra.points[skull_mask], method="nearest"
-    )
     disp: npt.NDArray[np.floating] = np.full(tetra.points.shape, np.nan)
-    disp[skull_mask] = disp_interp
-    mkit.io.save(output_file, tetra, point_data={"disp": disp})
+    disp[skull_mask] = scipy.interpolate.griddata(
+        np.vstack((pre_mandible.points, pre_maxilla.points)),
+        np.vstack(
+            (
+                post_mandible.points - pre_mandible.points,
+                post_maxilla.points - pre_maxilla.points,
+            )
+        ),
+        tetra.points[skull_mask],
+    )
+    validation: npt.NDArray[np.bool_] = np.full((len(tetra.points),), False)
+    validation[face_mask] = scipy.interpolate.griddata(
+        pre_face.points, pre_face.point_data["validation"], tetra.points[face_mask]
+    )
+
+    mkit.io.save(
+        output_file,
+        tetra,
+        point_data={"disp": disp, "validation": validation.astype(np.int8)},
+    )
+
+
+def tetgen(
+    face: trimesh.Trimesh,
+    mandible: trimesh.Trimesh,
+    maxilla: trimesh.Trimesh,
+) -> meshio.Mesh:
+    face_repaired: trimesh.Trimesh = mkit.ops.mesh_fix.mesh_fix(face)
+    mandible_repaired: trimesh.Trimesh = mkit.ops.mesh_fix.mesh_fix(mandible)
+    maxilla_repaired: trimesh.Trimesh = mkit.ops.mesh_fix.mesh_fix(maxilla)
+    skull_repaired: trimesh.Trimesh = trimesh.boolean.union(
+        [mandible_repaired, maxilla_repaired]
+    )
+    repaired: trimesh.Trimesh = trimesh.util.concatenate(
+        [face_repaired, skull_repaired]
+    )
+    hole: npt.NDArray[np.floating] = mkit.ops.ray.find_inner_point(skull_repaired)
+    tetra: meshio.Mesh = mkit.ops.tetgen.tetgen(
+        mkit.io.as_meshio(repaired, field_data={"holes": [hole]})
+    )
+    return tetra
+
+
+def extract_face_skull(
+    tetra: meshio.Mesh,
+) -> tuple[npt.NDArray[np.bool_], npt.NDArray[np.bool_]]:
+    boundary_faces: npt.NDArray[np.integer] = mkit.tetra.boundary_faces(
+        tetra.get_cells_type("tetra")
+    )
+    triangle = trimesh.Trimesh(tetra.points, boundary_faces)
+    face: trimesh.Trimesh
+    skull: trimesh.Trimesh
+    face, skull = triangle.split()
+    if np.prod(face.extents) < np.prod(skull.extents):
+        face, skull = skull, face
+    face_mask: npt.NDArray[np.bool_] = mkit.array.points.position_to_mask(
+        tetra.points, face.vertices
+    )
+    skull_mask: npt.NDArray[np.bool_] = mkit.array.points.position_to_mask(
+        tetra.points, skull.vertices
+    )
+    return face_mask, skull_mask
 
 
 if __name__ == "__main__":